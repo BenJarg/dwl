fded08b9c60e99517be68b12da71ad9133b39acb
Ben Jargowsky <benjar63@gmail.com>
2023-03-25 12:23:45 -0700

    Changed monocle symbol

--- dwl.c
+++ dwl.c
@@ -1817,7 +1817,7 @@ monocle(Monitor *m)
 		n++;
 	}
 	if (n)
-		snprintf(m->ltsymbol, LENGTH(m->ltsymbol), "[%d]", n);
+		snprintf(m->ltsymbol, LENGTH(m->ltsymbol), "(%d)", n);
 	if ((c = focustop(m)))
 		wlr_scene_node_raise_to_top(&c->scene->node);
 }
